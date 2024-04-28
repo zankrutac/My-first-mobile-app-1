c790cb3753bf9271684c8b8420a27340c3af14da
patelriyak <patelriya275@gmail.com>
2024-04-28 14:31:48 -0400

    components

---
+++ component/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from 'react-native';
+
+
+export default function Header() {
+    return (
+        <View style={styles.container}>
+            <Text>Header</Text>
+        </View>
+    );
+}
